ab7f9c4d69e706266fc01d0a79292ef48c6754b4
David Lawson <david.lawson.95@outlook.com>
2022-10-06 11:49:19 +1000

    changes

--- cmd/commands.go
+++ cmd/commands.go
@@ -14,12 +14,12 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-  Use:   "td",
-  Short: "Token Decoder is a cmd tool for decrypting A&G authentication tokens via your cli.",
-  Long: `A Fast and Flexible Token Decoder Generator built with
+	Use:   "td",
+	Short: "Token Decoder is a cmd tool for decrypting A&G authentication tokens via your cli.",
+	Long: `A Fast and Flexible Token Decoder Generator built with
                 love in Go.
                 Complete documentation is available in the repos readme.`,
-  Run: func(cmd *cobra.Command, args []string) {
+	Run: func(cmd *cobra.Command, args []string) {
 		usr, err := user.Current()
 		if err != nil {
 			panic(fmt.Errorf("fatal error: %w", err))
@@ -30,6 +30,7 @@ var rootCmd = &cobra.Command{
 			// path/to/whatever does not exist
 			os.MkdirAll(configDir, 0777)
 		}
+		fullConfigFilePath := filepath.Join(usr.HomeDir, "config.toml")
 
 		viper.SetDefault("Keys", []string{})
 		viper.SetConfigName("config")
@@ -38,15 +39,15 @@ var rootCmd = &cobra.Command{
 		err = viper.ReadInConfig()
 
 		if err != nil {
-			err := viper.WriteConfigAs("/Users/dlawson/.josectl/config.toml")
+			err := viper.WriteConfigAs(fullConfigFilePath)
 
 			if err != nil {
 				panic(fmt.Errorf("fatal error unable to create config.toml file: %w", err))
 			}
 		}
 
-		token, _ := cmd.Flags().GetString("token");
-		secret, _ := cmd.Flags().GetString("secret");
+		token, _ := cmd.Flags().GetString("token")
+		secret, _ := cmd.Flags().GetString("secret")
 
 		if token == "" {
 			fmt.Fprintln(os.Stderr, "Missing token to decode")
@@ -57,27 +58,27 @@ var rootCmd = &cobra.Command{
 			fmt.Fprintln(os.Stderr, "Missing secret to decode token with")
 			os.Exit(1)
 		}
-		
+
 		jwe, _ := jose.ParseEncrypted(token)
 		decryptedJWT, _ := jwe.Decrypt(secret)
 		jwtStr := string(decryptedJWT)
-		
+
 		tmp := jwtStr[strings.IndexByte(jwtStr, '.')+1:]
 		claim := tmp[:strings.IndexByte(tmp, '.')] + "=="
 
 		rawDecodedText, err := base64.StdEncoding.DecodeString(claim)
-    if err != nil {
-        panic(err)
-    }
+		if err != nil {
+			panic(err)
+		}
 		os.Stdout.Write(rawDecodedText)
-  },
+	},
 }
 
 func Execute() {
-  if err := rootCmd.Execute(); err != nil {
-    fmt.Fprintln(os.Stderr, err)
-    os.Exit(1)
-  }
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
 
 func init() {
